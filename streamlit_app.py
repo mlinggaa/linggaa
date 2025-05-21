3de0ba13618b25cbcc44413525bfdb68904ab659
mlinggaa <luarl9822@gmail.com>
2025-05-21 13:42:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Selamat Datang di Web Informatima")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ngodingseru bersama Bapak Hendri Setiadi"
 )
